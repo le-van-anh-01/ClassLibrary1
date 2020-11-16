b855673870825e4d881d186bf890aa61474485f6
le-van-anh-01 <le-van-anh@morien.com>
2020-11-16 09:47:40 +0900

    Update Class1.cs

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -8,5 +8,9 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+        public static string GetMessage()
+        {
+            return "こんにちは" + DateTime.Now.ToLongTimeString();
+        }
     }
 }
